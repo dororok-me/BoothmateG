cd9a32110cb1611e2850b65f9338e20c0673cd42
dororok <dororok@gmail.com>
2026-06-13 21:42:40 +0900

    Initial Commit

---
+++ BoothmateG/BoothmateGApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoothmateGApp.swift
+//  BoothmateG
+//
+//  Created by dororok on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BoothmateGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoothmateG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoothmateG
+//
+//  Created by dororok on 6/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
